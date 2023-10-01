85bfa8fcbcd0f5958924f3cec2b514ae185bf460
Mohamed KARIM <karimkar999@gmail.com>
2023-10-01 15:36:41 +0100

    Add error handler and style for (no data prompts

--- medium/weather-app/src/components/CitySearch.vue
+++ medium/weather-app/src/components/CitySearch.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="container w-full lg:w-[60rem] mx-auto px-1 mt-2">
     <input class="input w-full block mx-auto p-2 bg-transparent outline-none shadow-xl font-semibold tracking-wider" placeholder="search for a city" v-model="city" @input="getSearchResults" />
-    <ul class="mt-4 p-1 bg-slate-900 rounded-md flex flex-col" v-if="searchResult" >
+    <ul class="mt-4 p-1 bg-slate-900 rounded-md flex flex-col" v-if="searchResult.length" >
       <li
         class="p-2 hover:bg-slate-700 cursor-pointer rounded-md"
         v-for="result in searchResult" :key="result.id">
         {{result.place_name}}
       </li>
     </ul>
+    <p class="mt-4 p-2 bg-slate-900 rounded-md font-medium capitalize tracking-wide" v-else-if="error" >
+      something went wrong please try again!
+    </p>
+    <p class="mt-4 p-2 bg-slate-900 rounded-md font-medium capitalize tracking-wide" v-else-if="searchResult.length === 0">
+      no results found
+    </p>
   </div>
 </template>
 
@@ -19,10 +25,10 @@ const mapboxAPIKey =
   "pk.eyJ1Ijoiam9obmtvbWFybmlja2kiLCJhIjoiY2t5NjFzODZvMHJkaDJ1bWx6OGVieGxreSJ9.IpojdT3U3NENknF6_WhR2Q";
 const city = ref('');
 const queryTimeout = ref(null);
-const searchResult = ref(null)
-const error = ref('');
+const searchResult = ref([])
+const error = ref(null);
 
-const getSearchResults = () => {
+const getSearchResults = async () => {
   clearTimeout(queryTimeout.value);
   queryTimeout.value = setTimeout(async () => {
     if (city.value !== "") {
@@ -31,10 +37,9 @@ const getSearchResults = () => {
           `https://api.mapbox.com/geocoding/v5/mapbox.places/${city.value}.json?access_token=${mapboxAPIKey}&types=place&limit=5`
         );
         searchResult.value = res.data.features;
-      }
-      catch (err) {
+      } catch (err) {
         error.value = "Error fetching data from the API";
-        console.error(err);
+        console.log(err);
       }
     } else {
       searchResult.value = null;
